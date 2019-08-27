decent amount of changes, examples changed to account for gaussian generation function now being numpy, re-added pytorch functions, fixed the different averaging methods inputs, tests seem to pass on my end

kymatio/scattering3d/backend/torch_backend.py
@@ -45,18 +45,19 @@ def fft(input, inverse=False):
     return torch.fft(input, 3)
 
 
-def cdgmm3d(A, B):
+def cdgmm3d(A, B, inplace=False):
     """
     Pointwise multiplication of complex tensors.
 
     ----------
     A: complex torch tensor
     B: complex torch tensor of the same size as A
-
+    inplace : boolean, optional
+        if set True, all the operations are performed inplace
     Returns
     -------
     output : torch tensor of the same size as A containing the result of the 
-             elementwise complex multiplication of  A with B 
+             elementwise complex multiplication of A with B 
     """
     if not A.is_contiguous():
         warnings.warn("cdgmm3d: tensor A is converted to a contiguous array")
@@ -77,17 +78,32 @@ def cdgmm3d(A, B):
 
     if type(A) is not type(B):
         raise RuntimeError('A and B should be same type!')
+    
+    if A.device.type != B.device.type:
+        raise TypeError('A and B must be both on GPU or both on CPU.')
 
-    C = torch.empty_like(A)
+    if A.device.type == 'cuda':
+        if A.device.index != B.device.index:
+            raise TypeError('A and B must be on the same GPU!')
+
+
+
+
+    C = A.new(A.size())
 
     C[..., 0] = A[..., 0] * B[..., 0] - A[..., 1] * B[..., 1]
     C[..., 1] = A[..., 0] * B[..., 1] + A[..., 1] * B[..., 0]
 
-    return C
+    return C if not inplace else A.copy_(C)
 
 def finalize(s_order_1, s_order_2, max_order):
+    s_order_1 = [torch.stack([arr[...,0] for arr in scattering_coef], 1) for scattering_coef in s_order_1]
     if max_order == 2:
-        return torch.cat([torch.stack(s_order_1, dim=2), torch.stack(s_order_2, dim=2)], 1)
+        s_order_2 = [torch.stack([arr[...,0] for arr in scattering_coef], 1) for scattering_coef in s_order_2]
+
+
+        return torch.cat([torch.stack(s_order_1, dim=2), torch.stack(s_order_2,
+            dim=2)], 1)
     else:
         return torch.stack(s_order_1, dim=2)
 
@@ -120,7 +136,7 @@ def modulus_rotation(x, module):
         which is covariant to 3D translations and rotations
 
     """
-    if module == None:
+    if module is None:
         module = torch.zeros_like(x)
     else:
         module = module **2
@@ -128,6 +144,26 @@ def modulus_rotation(x, module):
     return torch.sqrt(module)
 
 
+def _fft_convolve(input_array, filter_array):
+        """
+        Computes the fourier space convolution of the input_array, 
+        given in signal space, with a filter, given in fourier space.
+
+        Parameters
+        ----------
+
+        input_array: torch tensor
+            size (batchsize, M, N, O, 2)
+        filter_array: torch tensor
+            size (M, N, O, 2)
+
+        Returns
+        -------
+
+        output: the result of the convolution of input_array with filter
+
+        """
+        return fft(cdgmm3d(fft(input_array, inverse=False), filter_array), inverse=True)
 
 
 def _low_pass_filter(input_array, low_pass):
@@ -146,6 +182,7 @@ def _low_pass_filter(input_array, low_pass):
     output: the result of input_array :math:`\\star phi_J`
 
     """
+
     return _fft_convolve(input_array, low_pass)
 
 
@@ -189,7 +226,7 @@ def _compute_local_scattering_coefs(input_array, low_pass, points):
 
     """
     local_coefs = torch.zeros(input_array.size(0), points.size(1), 1)
-    convolved_input = _low_pass_filter(input_array, low_pass)#j + 1)
+    convolved_input = _low_pass_filter(input_array, low_pass)
     for i in range(input_array.size(0)):
         for j in range(points[i].size(0)):
             x, y, z = points[i, j, 0], points[i, j, 1], points[i, j, 2]
@@ -286,3 +323,4 @@ backend.modulus_rotation = modulus_rotation
 backend.averaging = averaging
 backend.subsample = subsample
 backend.compute_integrals = compute_integrals
+backend._compute_standard_scattering_coefs = _compute_standard_scattering_coefs

kymatio/scattering3d/frontend/torch_frontend.py
@@ -17,6 +17,8 @@ import torch
 from kymatio.scattering3d.filter_bank import solid_harmonic_filter_bank, gaussian_filter_bank
 
 from kymatio.scattering3d.core.scattering3d import scattering3d
+from kymatio.scattering3d.utils import _apply_filters
+
 
 
 
@@ -97,20 +99,61 @@ class HarmonicScattering3DTorch(ScatteringTorch):
         methods = ['standard', 'local', 'integral']
         if (not self.method in methods):
             raise (ValueError('method must be in {}'.format(methods)))
-        if self.method == 'integral':
-            self.average =lambda x,j : compute_integrals(x[..., 0],j,'integral_powers')
+        if self.method == 'integral':\
+            self.average =lambda x,j : self.backend.compute_integrals(x[...,0],self.integral_powers)
         elif self.method == 'local':
-            self.average = lambda x,j:_compute_local_scattering_coefs(x, self.points, j)
+            self.average = lambda x,j:\
+                self.backend._compute_local_scattering_coefs(x,
+                        self.guassian_filters[j+1], self.points)
         elif self.method == 'standard':
-            self.average = lambda x, j: compute_global (x, self.points, j)
+            self.average = lambda x, j:\
+                self.backend._compute_standard_scattering_coefs(x, self.gaussian_filters[j], j, self.backend.subsample)
+
+    
+    def _apply(self, fn):
+        """
+            Mimics the behavior of the function _apply() of a nn.Module()
+        """
+        self.filters = _apply_filters(self.filters, fn)
+        self.gaussian_filters = fn(self.gaussian_filters)
+        return self
+
+    def cuda(self, device=None):
+        """
+            Mimics the behavior of the function cuda() of a nn.Module()
+        """
+        return self._apply(lambda t: t.cuda(device))
+
+    def to(self, *args, **kwargs):
+        """
+            Mimics the behavior of the function to() of a nn.Module()
+        """
+        device, dtype, non_blocking = torch._C._nn._parse_to(*args, **kwargs)
+
+        if dtype is not None:
+            if not dtype.is_floating_point:
+                raise TypeError('nn.Module.to only accepts floating point '
+                                'dtypes, but got desired dtype={}'.format(dtype))
+
+        def convert(t):
+            return t.to(device, dtype if t.is_floating_point() else None, non_blocking)
+
+        return self._apply(convert)
+
+    def cpu(self):
+        """
+            Mimics the behavior of the function cpu() of a nn.Module()
+        """
+        return self._apply(lambda t: t.cpu())
+
 
     def scattering(self, input):
         # TODO: Fill in needed code
         #
-        return scattering3d(input_array, filters=self.filters,
+        return scattering3d(input, filters=self.filters,
                 gaussian_filters=self.gaussian_filters, rotation_covariant=self.rotation_covariant, points=self.points, 
                 integral_powers=self.integral_powers, L=self.L, J=self.J, method=self.method, max_order=self.max_order, 
-                backend=self.backend, averaging=self.averaging)
+                backend=self.backend, averaging=self.average)
 
 
     def forward(self, input_array):