2f401658642170f9341d58a4322e7839f9dc1135
edouardoyallon <edouard.oyallon@ens.fr>
2019-08-28 12:22:35 +0100

    less computations

--- kymatio/scattering3d/core/scattering3d.py
+++ kymatio/scattering3d/core/scattering3d.py
@@ -23,40 +23,40 @@ def scattering3d(_input, filters, gaussian_filters, rotation_covariant, points, 
     finalize, fft, cdgmm3d, modulus, modulus_rotation = backend.finalize, backend.fft, backend.cdgmm3d, backend.modulus\
     , backend.modulus_rotation
 
+    U_0_c = fft(_input)
+
     s_order_1, s_order_2 = [], []
     for l in range(L+1):
         for j_1 in range(J+1):
-            frequency_1 = fft(_input)
             U_1_m = None
             if rotation_covariant:
                 for m in range(len(filters[l][j_1])):
-                    U_1_c = cdgmm3d(frequency_1, filters[l][j_1][m])
+                    U_1_c = cdgmm3d(U_0_c, filters[l][j_1][m])
                     U_1_c = fft(U_1_c, inverse=True)
                     U_1_m = modulus_rotation(U_1_c, U_1_m)
-
             else:
-                U_1_c = cdgmm3d(frequency_1, filters[l][j_1][0])
+                U_1_c = cdgmm3d(U_0_c, filters[l][j_1][0])
                 U_1_c = fft(U_1_c , inverse=True)
                 U_1_m = modulus(U_1_c)
 
-            S_1_l = averaging(U_1_m, j_1)
+            U_1_c = fft(U_1_m)
+            S_1_l = averaging(U_1_c, j_1)
             s_order_1.append(S_1_l)
 
             if max_order >1:
                 for j_2 in range(j_1+1, J+1):
-                    frequency_2 = fft(U_1_m)
                     U_2_m = None
-
                     if rotation_covariant:
                         for m in range(len(filters[l][j_1])):
-                            U_2_c = cdgmm3d(frequency_2, filters[l][j_1][m])
+                            U_2_c = cdgmm3d(U_1_c, filters[l][j_1][m])
                             U_2_c = fft(U_2_c, inverse=True)
                             U_2_m = modulus_rotation(U_2_c, U_2_m)
                     else:
-                        U_2_c = cdgmm3d(frequency_2, filters[l][j_1][0])
+                        U_2_c = cdgmm3d(U_1_c, filters[l][j_1][0])
                         U_2_c = fft(U_2_c, inverse=True)
                         U_2_m = modulus(U_2_c)
-                    S_2_l = averaging(U_2_m, j_2)
+                    U_2_c = fft(U_2_m)
+                    S_2_l = averaging(U_2_c, j_2)
                     s_order_2.append(S_2_l)
 
     return finalize(s_order_1, s_order_2, max_order)
