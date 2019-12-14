8784b9d6367e38a4a9b07956d797a2e8027c3d2f
chaudhm <chaudhm@wwu.edu>
2019-12-14 00:06:05 -0800

    TST make tests pass for calculating moments

--- kymatio/scatteringgraph/backend/torch_backend.py
+++ kymatio/scatteringgraph/backend/torch_backend.py
@@ -5,20 +5,20 @@ BACKEND_NAME = 'torch'
 
 def moment(x, q, mean=0, variance=1):
     "Calculate moment"
-    if mean == 0:
-        mean = torch.zeros(1, x.shape[1]).to(x.device())
+    if isinstance(mean, int):
+        mean = torch.zeros(1, x.shape[1]).to(x.device)
 
-    if variance == 1:
-        variance = torch.ones(1, x.shape[1]).to(x.device())
+    if isinstance(variance, int):
+        variance = torch.ones(1, x.shape[1]).to(x.device)
 
     x_mu = x - mean
     numerator = torch.pow(x_mu, q)
-    denominator = math.pow(variance, q) 
-    ratio = torch.divide(numerator, denominator)
-    q_th_moment = torch.sum(ratio, dim=1) / x.shape[0]
+    denominator = torch.pow(variance, q)    
+    ratio = torch.div(numerator, denominator)
+    q_th_moment = torch.sum(ratio, dim=0) / x.shape[0]
 
-    return q_th_moment
+    return q_th_moment.reshape(-1, 1)
 
 backend = namedtuple('backend', ['name', 'moment'])
 backend.name = 'torch'
-backend.momment = moment
+backend.moment = moment
