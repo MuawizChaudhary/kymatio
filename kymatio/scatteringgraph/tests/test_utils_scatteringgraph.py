788340b768b9398f2e6b4e177dd06d13b7842a78
chaudhm <chaudhm@wwu.edu>
2019-12-13 19:28:58 -0800

    TST added lazy random walk matrix calculation test

--- kymatio/scatteringgraph/tests/test_utils_scatteringgraph.py
+++ kymatio/scatteringgraph/tests/test_utils_scatteringgraph.py
@@ -5,25 +5,64 @@ from kymatio.scatteringgraph.utils import compute_degree_vector
 class TestComputeDegreeVector:
     def test_compute_degree_vector(self):
         # node A direct connection to node B
-        A_1 = np.array([[0, 1], [0, 0]])
-        deg_A_1 = np.array([1, 0]).reshape(-1, 1)
+        A = np.array([[0, 1], [0, 0]])
+        deg_A = np.array([1, 0]).reshape(-1, 1)
 
-        degree_vector_A_1 = compute_degree_vector(A_1)
+        degree_vector_A = compute_degree_vector(A)
 
-        assert np.allclose(degree_vector_A_1, deg_A_1)
+        assert np.allclose(degree_vector_A, deg_A)
 
         # two clique adjacency matrix
-        A_2 = np.array([[0, 1], [1, 0]])
-        deg_A_2 = np.array([1,1]).reshape(-1, 1)
+        A = np.array([[0, 1], [1, 0]])
+        deg_A = np.array([1,1]).reshape(-1, 1)
 
-        degree_vector_A_2 = compute_degree_vector(A_2)
+        degree_vector_A = compute_degree_vector(A)
 
-        assert np.allclose(degree_vector_A_2, deg_A_2)
+        assert np.allclose(degree_vector_A, deg_A)
         
         # three clique adjavency matrix  
-        A_3 = np.array([[0, 1, 1], [1, 0, 1], [1, 1, 0]])
-        deg_A_3 = np.array([2, 2, 2]).reshape(-1, 1)
+        A = np.array([[0, 1, 1], [1, 0, 1], [1, 1, 0]])
+        deg_A = np.array([2, 2, 2]).reshape(-1, 1)
 
-        degree_vector_A_3 = compute_degree_vector(A_3)
+        degree_vector_A = compute_degree_vector(A)
 
-        assert np.allclose(degree_vector_A_3, deg_A_3)
+        assert np.allclose(degree_vector_A, deg_A)
+
+    def test_compute_lazy_walk_matrix(self):
+        I2 = np.eye(2)
+        I3 = np.eye(3)
+        
+
+        # two clique adjacency matrix
+        A = np.array([[0, 1], [1, 0]])
+        degree_vector_A = compute_degree_vector(A).reshape(-1,)
+        
+        D = np.diag(degree_vector_A)
+        D_i = np.linalg.inv(D)
+
+        AD_i = np.dot(A, D_i)
+
+        P = np.array([[1/2, 1/2], [1/2, 1/2]])
+
+        assert np.allclose(P, (1/2) * (I2 + AD_i))
+
+        # three clique adjacency matrix
+        A = np.array([[0, 1, 1], [1, 0, 1], [1, 1, 0]])
+        degree_vector_A = compute_degree_vector(A).reshape(-1,)
+        
+        D = np.diag(degree_vector_A)
+        D_i = np.linalg.inv(D)
+
+        AD_i = np.dot(A, D_i)
+
+        P = np.array([[1/2, 1/4, 1/4], [1/4, 1/2, 1/4], [1/4, 1/4, 1/2]])
+
+        assert np.allclose(P, (1/2) * (I3 + AD_i))
+
+
+
+
+
+        #P = np.array([[1/2, 1/4, 1/4], [], []]) 
+
+
