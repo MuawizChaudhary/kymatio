08e641ae2e0dc9197422bfc609d1170b61ae6769
chaudhm <chaudhm@wwu.edu>
2019-12-15 15:46:40 -0800

    TST changed computation from using floats to doubles. added scatteringraph calculation

--- kymatio/scatteringgraph/tests/test_torch_scatteringgraph.py
+++ kymatio/scatteringgraph/tests/test_torch_scatteringgraph.py
@@ -2,6 +2,7 @@ import pytest
 import numpy as np
 import torch
 from kymatio.scatteringgraph.utils import compute_degree_vector
+from kymatio.scatteringgraph import ScatteringGraph
 
 devices = ['cpu']
 if torch.cuda.is_available():
@@ -30,18 +31,18 @@ class TestMoment:
         moment = backend.normalized_moment 
 
         A = torch.from_numpy(A)
-        x = torch.from_numpy(degree_vector_A).to(device)
+        x = torch.from_numpy(degree_vector_A).to(device).double()
 
         #q = 1
         mean = np.array([[1]])
-        mean = torch.from_numpy(mean).float()
+        mean = torch.from_numpy(mean).double()
 
         mean_moment = moment(x, 1)
         assert torch.allclose(mean, mean_moment)
 
         #q = 2
         var = np.array([[0]])
-        var = torch.from_numpy(var).float()
+        var = torch.from_numpy(var).double()
 
         var_moment = moment(x, 2, mean=mean_moment)
         assert torch.allclose(var, var_moment)
@@ -53,18 +54,18 @@ class TestMoment:
         A = np.array([[0, 1, 1], [1, 0, 1], [1, 1, 0]])
         degree_vector_A = compute_degree_vector(A)
 
-        x = torch.from_numpy(degree_vector_A).to(device)
+        x = torch.from_numpy(degree_vector_A).to(device).double()
 
         #q = 1
         mean = np.array([[2]])
-        mean = torch.from_numpy(mean).float()
+        mean = torch.from_numpy(mean).double()
 
         mean_moment = moment(x, 1)
         assert torch.allclose(mean, mean_moment)
 
         #q = 2
         var = np.array([[0]])
-        var = torch.from_numpy(var).float()
+        var = torch.from_numpy(var).double()
 
         var_moment = moment(x, 2, mean=mean_moment)
         assert torch.allclose(var, var_moment)
@@ -83,18 +84,18 @@ class TestMoment:
         moment = backend.unnormalized_moment 
 
         A = torch.from_numpy(A)
-        x = torch.from_numpy(degree_vector_A).to(device).float()
+        x = torch.from_numpy(degree_vector_A).to(device).double()
 
         #q = 1
         mean = np.array([[2]])
-        mean = torch.from_numpy(mean).float()
+        mean = torch.from_numpy(mean).double()
 
         mean_moment = moment(x, 1)
         assert torch.allclose(mean, mean_moment)
 
         #q = 2
         var = np.array([[2]])
-        var = torch.from_numpy(var).float()
+        var = torch.from_numpy(var).double()
 
         var_moment = moment(x, 2)
         assert torch.allclose(var, var_moment)
@@ -106,21 +107,46 @@ class TestMoment:
         A = np.array([[0, 1, 1], [1, 0, 1], [1, 1, 0]])
         degree_vector_A = compute_degree_vector(A)
 
-        x = torch.from_numpy(degree_vector_A).to(device).float()
+        x = torch.from_numpy(degree_vector_A).to(device).double()
 
         #q = 1
         mean = np.array([[6]])
-        mean = torch.from_numpy(mean).float()
+        mean = torch.from_numpy(mean).double()
 
         mean_moment = moment(x, 1)
         assert torch.allclose(mean, mean_moment)
 
         #q = 2
         var = np.array([[12]])
-        var = torch.from_numpy(var).float()
+        var = torch.from_numpy(var).double()
 
         var_moment = moment(x, 2)
         assert torch.allclose(var, var_moment)
 
         #q = 3
         #come up with a better example than cliques
+
+class TestScatteringGraph:
+    @pytest.mark.parametrize('backends_devices', backends_devices)
+    def test_scattering_graph(self, backends_devices):
+        backend, device = backends_devices
+        
+        # two clique adjacency matrix
+        A = np.array([[0, 1], [1, 0]])
+
+        S = ScatteringGraph(J=2, Q=2, A=A, normalize=False, max_order=2,
+                backend=backend)
+        S = S.to(device)
+
+        x = compute_degree_vector(A)
+        x = torch.from_numpy(x).to(device).double()
+
+        S_x = S(x)
+        print(S_x.shape)
+        print(S_x)
+        assert False
+
+
+
+        
+
