71f3805978e4673bf3042706170f0165634d9756
chaudhm <chaudhm@wwu.edu>
2020-07-21 06:39:33 -0700

    MAINT tensorflow now inheriates from numpy backend

--- kymatio/backend/tensorflow_backend.py
+++ kymatio/backend/tensorflow_backend.py
@@ -1,49 +1,18 @@
-import numpy as np
+from .numpy_backend import NumpyBackend
 import tensorflow as tf
 
 
-class Modulus():
-    """This class implements a modulus transform for complex numbers.
-
-    Parameters
-    ----------
-    x: input complex tensor.
-
-    Returns
-    ----------
-    output: a complex tensor equal to the modulus of x.
-
-    Usage
-    ----------
-    modulus = Modulus()
-    x_mod = modulus(x)
-    """
-    def __call__(self, x):
-        norm = tf.abs(x)
-
-        return norm
-
-class TensorFlowBackend:
+class TensorFlowBackend(NumpyBackend):
     def __init__(self):
-        self.name = 'tensorflow'
-        self.modulus = Modulus()
+        super(TensorFlowBackend, self).__init__(name='tensorflow')
 
-    def complex_check(self, x):
-        if not self._is_complex(x):
-            raise TypeError('The input should be complex.')
-    
-    def real_check(self, x):
-        if not self._is_real(x):
-            raise TypeError('The input should be real.')
-    
-    def _is_complex(self, x):
-        return (x.dtype == np.complex64) or (x.dtype == np.complex128)
-    
-    def _is_real(self, x):
-        return (x.dtype == np.float32) or (x.dtype == np.float64)
-  
     def concatenate(self, arrays):
         return tf.stack(arrays, axis=1)
+
+    def modulus(self, x):
+        norm = tf.abs(x)
+        
+        return norm
     
     def cdgmm(self, A, B):
         """
