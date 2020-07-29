7e14ac10f48c26be6b1ce9fa072381f74a7b8f78
chaudhm <chaudhm@wwu.edu>
2020-07-29 02:09:45 -0700

    MAINT corrected calcualtion of fisher matrix

--- examples/3d/wavelet.py
+++ examples/3d/wavelet.py
@@ -1,25 +1,66 @@
 from kymatio.scattering3d.frontend.torch_frontend import WaveletTorch3D
 import numpy as np
 import torch
+import glob
+import pandas
 
 orientations = np.array([(1,0,0), (0, 1,0), (0, 0, 1), (1,1,0), (0, 1,1), (1,
     0, 1), (1, 1, 1)])
 
-x = torch.randn((1, 64, 64, 64)).cuda().double() #torch.zeros((1, 64, 64, 64)).cuda().double()
-#x[:, 16:48, 16:48, 16:48] = 1
+# glob to get all the cubes
+filenames = glob.glob('../../../*/*/df_m_64_z=0.npy')
+filenames.sort()
 
-y = torch.autograd.Variable(x, requires_grad=True)
+data = []
 
-S = WaveletTorch3D(3, (64, 64, 64), orientations, rho='relu', return_list=False,
-        subsample=False).cuda().double()
+# load cubes into numpy
+for filename in filenames:
+    data.append(np.load(filename))
 
-Sx = S(y)
-backend = S.backend
+data = np.array(data)
 
-mean = backend.mean(Sx).reshape(Sx.shape[0], -1)
+with torch.no_grad():
+    S = WaveletTorch3D(3, (64, 64, 64), orientations, rho='relu', return_list=False,
+            subsample=False).cuda().double()
 
-cov = backend.covariance(mean, torch.mean(mean, dim=-1, keepdim=True))
-i_cov = torch.inverse(cov)
+    # number of training iterations x batch size x spatial dim
+    data = data.reshape(-1, 2, 64, 64, 64)
 
-#fisher_1 = torch.matmul(i_cov, grad)
-#fisher_2 = torch.matmul(grad.t(), fisher_1)
+    # total number of cubes x scattering features
+    phase_harmonics = torch.empty((1000, 84)).cuda()
+
+    counter = 0
+    for i in data:
+        # x.shape = batchsize x spatial dim
+        x = torch.from_numpy(i).double().cuda()
+
+        # Sx.shape =  batchsize x wavelet coefficents x phases x spatial dim
+        Sx = S(x)
+
+        backend = S.backend
+
+        # mean.shape = batchsize x wavelet coefficents*phases
+        mean = Sx.mean((-3, -2, -1)).reshape(Sx.shape[0], -1)
+        phase_harmonics[counter : counter + i.shape[0]] = mean
+
+        counter += i.shape[0]
+    
+    # centered_phase_harmonics.shape = dataset size x wavelet coefficents*phases
+    centered_phase_harmonics = phase_harmonics - phase_harmonics.mean(0)
+
+    # wavelet coefficents*phases x wavelet coefficents*phases
+    cov = centered_phase_harmonics.t() @ centered_phase_harmonics
+
+    # 500 x num_hyperparameters x calculation orientations x wavelet coefficents*phases
+    phase_harmonics = phase_harmonics.reshape(500, 1, 2, -1)
+
+    grad = phase_harmonics[..., 0, :] - phase_harmonics[..., 1, :]
+    grad = grad.reshape(500, 84)
+
+    cov_i = torch.inverse(cov + torch.eye(84).cuda())
+
+    # 500 x 500
+    # I should do this batch by batch perhaps
+    fisher_1 = torch.matmul(cov_i, grad.t())
+    fisher_2 = torch.matmul(grad, fisher_1)
+
