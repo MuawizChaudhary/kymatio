281f0a9a0ca3e26f92b08d3ebb3d0350f16a21e8
Muawiz Chaudhary <muawizc@gmail.com>
2022-06-16 01:24:40 -0400

    STY remove comments

--- kymatio/scattering3d/backend/numpy_backend.py
+++ kymatio/scattering3d/backend/numpy_backend.py
@@ -47,10 +47,6 @@ class NumpyBackend3D(NumpyBackend):
                 Tensor of size (B, P) containing the integrals of the input_array
                 to the powers p (l_p norms).
         """
-        # integrals = cls._np.zeros((input_array.shape[0], len(integral_powers)),
-        #         dtype=cls._np.complex64)
-        # for i_q, q in enumerate(integral_powers):
-        #     integrals[:, i_q] = (input_array ** q).reshape((input_array.shape[0], -1)).sum(axis=1)
         integrals = []
         for i_q, q in enumerate(integral_powers):
             integrals.append((input_array ** q).reshape((input_array.shape[0], -1)).sum(axis=1))
